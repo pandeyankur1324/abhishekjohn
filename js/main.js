d95335a3cc8a67d1555dcf03d896f6e9b347b6b0
ankur1324 <pandeyankur55143@gmail.com>
2023-07-18 19:09:53 +0530

    Changes in website
    Navbar has been changed along with it some new blocks have been introduced.

--- js/main.js
+++ js/main.js
@@ -1,93 +1,56 @@
 let index = 0;
 const totalWorkItems = $(".work-item").length;
 
+$(function () {
+  $(window).scroll(function () {
+    var top_offset = $(window).scrollTop();
+    if (top_offset < 50) {
+      $(".navigation").removeClass("sticky");
+    }
+  });
+});
+
 $(window).on("load", function () {
   $(".preloader").addClass("loaded");
 });
 
-$(document).ready(function () {
-  // navtoggle
-  $(".nav-toggle").click(function () {
-    $(".header .nav").slideToggle();
-  });
-  $(".header .nav a").click(function () {
-    if ($(window).width() < 768) {
-      $(".header .nav").slideToggle();
-    }
-  });
+// set lightbox img max height
+const wHeight = $(window).height();
+$(".lightbox-img").css("height", wHeight + "px");
 
-  //fixed header
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".header").addClass("fixed");
-    } else {
-      $(".header").removeClass("fixed");
-    }
-  });
+//lightbox
+$(".work-item-inner").click(function () {
+  index = $(this).parent(".work-item").index();
+  $(".lightbox").addClass("open");
+  lightboxSlideShow();
+});
+$(".lightbox .prev").click(function () {
+  if (index == 0) {
+    index = totalWorkItems - 1;
+  } else {
+    index--;
+  }
+  lightboxSlideShow();
+});
+$(".lightbox .next").click(function () {
+  if (index == totalWorkItems - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  lightboxSlideShow();
+});
 
-  // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+//close lightbox
+$(".lightbox-close").click(function () {
+  $(".lightbox").removeClass("open");
+});
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
-
-  // set lightbox img max height
-  const wHeight = $(window).height();
-  $(".lightbox-img").css("height", wHeight + "px");
-
-  //lightbox
-  $(".work-item-inner").click(function () {
-    index = $(this).parent(".work-item").index();
-    $(".lightbox").addClass("open");
-    lightboxSlideShow();
-  });
-  $(".lightbox .prev").click(function () {
-    if (index == 0) {
-      index = totalWorkItems - 1;
-    } else {
-      index--;
-    }
-    lightboxSlideShow();
-  });
-  $(".lightbox .next").click(function () {
-    if (index == totalWorkItems - 1) {
-      index = 0;
-    } else {
-      index++;
-    }
-    lightboxSlideShow();
-  });
-
-  //close lightbox
-  $(".lightbox-close").click(function () {
-    $(".lightbox").removeClass("open");
-  });
-
-  //close lightbox when clicked outside of img-box
-  $(".lightbox").click(function (event) {
-    if ($(event.target).hasClass("lightbox")) {
-      $(this).removeClass("open");
-    }
-  });
+//close lightbox when clicked outside of img-box
+$(".lightbox").click(function (event) {
+  if ($(event.target).hasClass("lightbox")) {
+    $(this).removeClass("open");
+  }
 });
 
 function lightboxSlideShow() {
@@ -97,3 +60,75 @@ function lightboxSlideShow() {
   $(".lightbox-category").html(category);
   $(".lightbox-counter").html(totalWorkItems + "/" + (index + 1));
 }
+
+// navigation bar
+(function ($) {
+  $(function () {
+    //  open and close nav
+    $("#navbar-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
+
+    // Hamburger toggle
+    $("#navbar-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
+
+    // If a link has a dropdown, add sub menu toggle.
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".navbar-dropdown").slideToggle("slow");
+
+      // Close dropdown when select another dropdown
+      $(".navbar-dropdown").not($(this).siblings()).hide("slow");
+      e.stopPropagation();
+    });
+
+    // Click outside the dropdown will remove the dropdown class
+    $("html").click(function () {
+      $(".navbar-dropdown").hide();
+    });
+  });
+})(jQuery);
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.querySelector(".navigation");
+console.log(navigation);
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+console.log(sticky);
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//Scroll Animation
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: "smooth",
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: "smooth",
+});
+
+// Scroll to a certain element
+document.querySelector(".hello").scrollIntoView({
+  behavior: "smooth",
+});
