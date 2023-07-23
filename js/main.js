0add4292e4636a0608c98f9c8780fb558fadefd3
ankur1324 <pandeyankur55143@gmail.com>
2023-07-23 11:10:27 +0530

    Form Update
    Form in now Working

--- js/main.js
+++ js/main.js
@@ -211,3 +211,43 @@ document.addEventListener("DOMContentLoaded", function () {
   // Change the image at regular intervals
   setInterval(changeImage, interval);
 });
+
+//Form submission
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        setTimeout(() => {
+          status.innerHTML = "";
+        }, 4000);
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
